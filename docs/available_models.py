514ec27832280964cb003bbb1488813289c3863a
retarfi <b2019msuzuki@socsim.org>
2022-09-27 09:05:18 +0900

    doc: add available models automatically

---
+++ docs/available_models.py
@@ -0,0 +1,26 @@
+import os
+import sys
+from typing import Dict, Union
+
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append("../src/jptranstokenizer/")
+
+import tokenization_utils
+
+d: Dict[str, Dict[str, Union[str, bool]]] = tokenization_utils.PUBLIC_AVAILABLE_SETTING_MAP
+
+text: str = """*******************************
+Available Pretrained Models
+*******************************
+
+{}
+
+"""
+
+output: str = text.format("".join(map(lambda x: "* {}\n".format(x), sorted(d.keys()))))
+
+with open("source/available_models.rst", "w", encoding="utf-8") as f:
+    f.write(output)
+
+
