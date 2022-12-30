7ca51da34cc5bdc4987acd5ec541a292a98d4b42
retarfi <b2019msuzuki@socsim.org>
2022-12-30 13:17:37 +0900

    test: add japanesetransformertokenizer class
    In accordance with this, some vocabulary files are also added.
    vocab.txt and spiece.model is created with sudachi-C word splitter.
    bert-small-japanese is cloned from
    https://huggingface.co/izumi-lab/bert-small-japanese

--- tests/test_tokenization_utils.py
+++ tests/test_tokenization_utils.py
@@ -1,7 +1,15 @@
+import os.path
 from typing import Tuple
 
-from src.jptranstokenizer.tokenization_utils import get_word_tokenizer
+import pytest
+
+from src.jptranstokenizer.tokenization_utils import (
+    get_word_tokenizer,
+    JapaneseTransformerTokenizer,
+)
 from src.jptranstokenizer.mainword.base import MainTokenizerABC
+
+DATA_DIR: str = os.path.join(os.path.dirname(__file__), "data")
 
 
 def test_get_word_tokenizer() -> None:
@@ -17,3 +25,47 @@ def test_get_word_tokenizer() -> None:
         _: MainTokenizerABC = get_word_tokenizer(
             word_tokenizer_type=word_tokenizer_type, do_lower_case=False
         )
+
+
+@pytest.mark.parametrize(
+    "word_tokenizer_type,subword_tokenizer_type",
+    [
+        ("basic", "wordpiece"),
+        ("mecab", "wordpiece"),
+        ("juman", "wordpiece"),
+        ("sudachi", "sentencepiece"),
+        ("spacy-luw", "sentencepiece"),
+        ("none", "sentencepiece"),
+    ],
+)
+def test_japanesetransformertokenizer_init(
+    word_tokenizer_type: str, subword_tokenizer_type: str
+) -> None:
+
+    vocab_file: str
+    if subword_tokenizer_type == "wordpiece":
+        vocab_file = os.path.join(DATA_DIR, "wordpiece/vocab.txt")
+    elif subword_tokenizer_type == "sentencepiece":
+        vocab_file = os.path.join(DATA_DIR, "sentencepiece/spiece.model")
+    else:
+        raise ValueError("Invalid subword_tokenizer_type")
+    JapaneseTransformerTokenizer(
+        vocab_file=vocab_file,
+        word_tokenizer_type=word_tokenizer_type,
+        subword_tokenizer_type=subword_tokenizer_type,
+    )
+
+
+@pytest.mark.parametrize(
+    "tokenizer_class,vocab_dir",
+    [("AlbertTokenizer", "sentencepiece/"), ("BertJapaneseTokenizer", "wordpiece/")],
+)
+def test_japanesetransformertokenizer_from_pretrained(
+    tokenizer_class: str, vocab_dir: str
+) -> None:
+    tokenizer_name_or_path: str = os.path.join(DATA_DIR, vocab_dir)
+    JapaneseTransformerTokenizer.from_pretrained(
+        tokenizer_class=tokenizer_class,
+        tokenizer_name_or_path=tokenizer_name_or_path,
+        word_tokenizer_type="sudachi",
+    )
